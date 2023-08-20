9c17c3c804f817796ae45caf9457a0cede313e2d
THINKPADT480S\Admin <artlebedev2006@gmail.com>
2023-08-20 13:09:32 +0300

    try to fix

--- messages_microservice/src/main/java/repository/DialogRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import models.Dialog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface DialogRepository extends JpaRepository<Dialog, UUID> {
-}
