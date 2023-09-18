06226983c323572b3a3638d1049c5aa6f8a34edb
Artem Lebedev <artlebedev2006@gmail.com>
2023-09-18 21:20:41 +0300

    ADD /

--- src/main/java/ru/skillbox/socialnetwork/messages/repository/DialogRepository.java
+++ src/main/java/ru/skillbox/socialnetwork/messages/repository/DialogRepository.java
@@ -1,9 +1,16 @@
 package ru.skillbox.socialnetwork.messages.repository;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import ru.skillbox.socialnetwork.messages.models.DialogModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface DialogRepository extends JpaRepository<DialogModel, UUID> {
+	@NotNull
+	Page<DialogModel> findAllByConversationPartner1(Long conversationPartner1, @NotNull Pageable pageable);
+	Boolean existsByConversationPartner1AndConversationPartner2(Long conversationPartner1, Long conversationPartner2);
+
 }
